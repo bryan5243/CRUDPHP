29e87b454889114693381b10e518b7d05419adf9
unknown <chuchei5243@gmail.com>
2023-05-12 22:08:31 -0500

    listado

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" coentnt="IE=edge">
+    <meta name="viewport" content="width=dviece-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Php</title>
     <style>
@@ -21,7 +21,7 @@
             padding: 8px;
         }
 
-        #customers tR:nth-child(even) {
+        #customers tr:nth-child(even) {
             background-color: #f2f2f2;
         }
 
@@ -60,6 +60,8 @@
                 <?php include('registrar.php')    ?>
             </div>
             <div class="col-sm-8">
+
+                <?php include('listado.php')    ?>
 
 
             </div>
---
+++ listado.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de registros</title>
+
+</head>
+
+<body>
+
+    <table id="customers">
+        <tr>
+            <th>N°</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Direccion</th>
+            <th>Telefono</th>
+            <th>Opciones</th>
+
+        </tr>
+        <?php
+        include("conectarbd.php");
+        $sql = "select * from alumnos";
+        $resultado = mysqli_query($conexion, $sql);
+        while ($row = $resultado->fetch_assoc()) {
+            $field0name = $row['id'];
+            $field1name = $row['nombre'];
+            $field2name = $row['apellido'];
+            $field3name = $row['direccion'];
+            $field4name = $row['telefono'];
+
+            echo '<tr>
+<td>' . $field0name . '</td>
+<td>' . $field1name . '</td>
+<td>' . $field2name . '</td>
+<td>' . $field3name . '</td>
+<td>' . $field4name . '</td>
+<td>
+<a class="btn btn-danger">Eliminar</a>
+<button class="btn btn-primary">Actualizar</button>
+
+</td>
+
+</tr>';
+        } ?>
+    </table>
+
+</body>
+
+</html>
